13ef178117b15a748b94c5e1ce1efd35ec7ccccb
devbird <devbird@qq.com>
2016-08-06 14:49:14 +0800

    tt

--- androidframework/src/main/java/com/blankj/androidframework/utils/ScreenUtils.java
+++ androidframework/src/main/java/com/blankj/androidframework/utils/ScreenUtils.java
@@ -165,20 +165,4 @@ public class ScreenUtils {
         return km.inKeyguardRestrictedInputMode();
     }
 
-
-     /**
-     * 判断是否存在状态栏
-     *
-     * @param activity
-     * @return false: 不存在  ；true: 存在
-     */
-    public static boolean isExistStatusBar(Activity activity) {
-        WindowManager.LayoutParams params = activity.getWindow().getAttributes();
-        if ((params.flags & WindowManager.LayoutParams.FLAG_FULLSCREEN)
-                == WindowManager.LayoutParams.FLAG_FULLSCREEN) {
-            return false;
-        } else {
-            return true;
-        }
-    }
 }
