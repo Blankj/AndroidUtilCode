829c675cdd341cf449e1e2f9cc15ae3613c81c4d
Emre Köse <e.kose26@gmail.com>
2016-09-22 23:39:41 +0300

    getBytesFromFile added to FileUtils.java

--- utilcode/src/main/java/com/blankj/utilcode/utils/FileUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/utils/FileUtils.java
@@ -1088,4 +1088,35 @@ public class FileUtils {
         if (lastPoi == -1 || lastSep >= lastPoi) return "";
         return filePath.substring(lastPoi);
     }
+
+    /**
+     * Returns a byte array of a file
+     *
+     * @param file
+     * @return byte[]
+     * @throws IOException
+     */
+    public static byte[] getBytesFromFile( File file ) throws IOException {
+        InputStream is = new FileInputStream( file );
+        long length = file.length();
+
+        if ( length > Integer.MAX_VALUE ) {
+            throw new IOException( "File is too large" );
+        }
+
+        byte[] bytes = new byte[(int) length];
+
+        int offset = 0;
+        int numRead = 0;
+        while ( offset < bytes.length && ( numRead = is.read( bytes, offset, bytes.length - offset ) ) >= 0 ) {
+            offset += numRead;
+        }
+
+        if ( offset < bytes.length ) {
+            throw new IOException( "Could not completely read file " + file.getName() );
+        }
+        is.close();
+        return bytes;
+    }
+
 }
