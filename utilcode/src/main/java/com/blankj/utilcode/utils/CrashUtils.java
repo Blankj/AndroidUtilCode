227f7e2cd1fffb94198fde1cbf0367cad7d3a247
cmj <625783482@qq.com>
2016-10-02 17:35:33 +0800

    see 10/02 log

--- utilcode/src/main/java/com/blankj/utilcode/utils/CrashUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/utils/CrashUtils.java
@@ -1,15 +1,23 @@
 package com.blankj.utilcode.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.nio.channels.NonWritableChannelException;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * <pre>
@@ -21,140 +29,94 @@ import java.lang.Thread.UncaughtExceptionHandler;
  */
 public class CrashUtils implements Thread.UncaughtExceptionHandler {
 
+    private volatile static CrashUtils mInstance;
+    private Context mContext;
     private UncaughtExceptionHandler mHandler;
-    private String mAndroidVersion;
-    private String mModel;
-    private String mManufacturer;
-    public static String sVERSION = "Unknown";
-    private static CrashBuilder sBuilder;
-    private boolean isAppend;
-    private boolean isSimple;
+    private boolean mInitialized;
 
-    private CrashUtils() {
-        mHandler = Thread.currentThread().getUncaughtExceptionHandler();
-        Thread.currentThread().setUncaughtExceptionHandler(this);
-        mAndroidVersion = Build.VERSION.RELEASE;//安卓系统版本
-        mModel = Build.MODEL;// 设备型号
-        mManufacturer = Build.MANUFACTURER;// 设备厂商
+    private CrashUtils(){
+
     }
 
     /**
-     * @param isAppend 是否为日志追加模式
+     * 获取单例
+     *
+     * @return 单例
      */
-    public CrashUtils setAppend(boolean isAppend) {
-        this.isAppend = isAppend;
-        return this;
-    }
-
-    /**
-     * @param isSimple 是否为简单的日志记录模式
-     */
-    public CrashUtils setSimple(boolean isSimple) {
-        this.isSimple = isSimple;
-        return this;
-    }
-
-
-    public static CrashUtils init(Context context, String dirName) {
-        try {
-            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            sVERSION = info.versionName + info.versionCode;
-            sBuilder = CrashBuilder.build(context, dirName);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    public static CrashUtils getInstance() {
+        synchronized (CrashUtils.class) {
+            if (null == mInstance) {
+                mInstance = new CrashUtils();
+            }
         }
-        return new CrashUtils();
+        return mInstance;
     }
 
-//    public static String formatNumber(int value) {
-//        return new DecimalFormat("00").format(value);
-//    }
-//
-//    public static String getCurrentDate() {
-//        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT+08:00"));
-//        return calendar.get(Calendar.YEAR) + "-" + formatNumber((calendar.get(Calendar.MONTH) + 1)) + "-"
-//                + formatNumber(calendar.get(Calendar.DAY_OF_MONTH)) + "  "
-//                + formatNumber(calendar.get(Calendar.HOUR_OF_DAY)) + ":" + formatNumber(calendar.get(Calendar
-// .MINUTE));
-//    }
+    /**
+     * 初始化
+     *
+     * @param context 上下文
+     */
+    public void init(Context context) {
+        if (mInitialized) return;
+        mInitialized = true;
+        mContext = context;
+        mHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
 
     @Override
     public void uncaughtException(Thread thread, Throwable throwable) {
-        if (!isAppend) {
-            FileUtils.createFileByDeleteOldFile(sBuilder.getCarsh_log());
+        String dir;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            dir = mContext.getExternalCacheDir().getPath();
         } else {
-            FileUtils.createOrExistsFile(sBuilder.getCarsh_log());
+            dir = mContext.getCacheDir().getPath();
         }
-        File file = new File(sBuilder.getCarsh_log());
+        String fullPath = dir + File.separator + "crash_" + TimeUtils.getCurTimeString() + ".txt";
+        if (!FileUtils.createOrExistsFile(fullPath)) return;
+        StringBuilder sb = new StringBuilder();
+        sb.append(getCrashHead());
+        Writer writer = new StringWriter();
         PrintWriter pw = null;
         try {
-            pw = new PrintWriter(new FileWriter(file, true));
-            pw.write("\n*************---------Crash  Log  Head ------------****************\n\n");
-            pw.write("Happened Time: " + TimeUtils.getCurTimeString() + "\n");
-            pw.write("Android Version: " + mAndroidVersion + "\n");
-            pw.write("Device Model: " + mModel + "\n");
-            pw.write("Device Manufacturer: " + mManufacturer + "\n");
-            pw.write("App Version: v" + sVERSION + "\n\n");
-            pw.write("*************---------Crash  Log  Head ------------****************\n\n");
-            if (!isSimple)
-                throwable.printStackTrace(pw);
-            else {
-                pw.write(throwable.getLocalizedMessage() + "\n");
+            pw = new PrintWriter(writer);
+            throwable.printStackTrace(pw);
+            Throwable cause = throwable.getCause();
+            while (cause != null) {
+                cause.printStackTrace(pw);
+                cause = cause.getCause();
             }
-        } catch (IOException e) {
-            return;
         } finally {
             FileUtils.closeIO(pw);
         }
-        FileUtils.createFileByDeleteOldFile(sBuilder.getCrash_tag());
+        sb.append(writer.toString());
+        FileUtils.writeFileFromString(fullPath, sb.toString(), false);
         if (mHandler != null) {
             mHandler.uncaughtException(thread, throwable);
         }
     }
 
-    public static class CrashBuilder {
-        private String carsh_dir;
-        public String getCrash_dir() {
-            return carsh_dir;
-        }
-        public String getCarsh_log() {
-            return getCrash_dir() + File.separator + "carshRecord.log";
-        }
-        public String getCrash_tag() {
-            return getCrash_dir() + File.separator + ".carshed";
-        }
-        public CrashBuilder(Context context, String dirName) {
-            if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-                this.carsh_dir = context.getCacheDir().getPath() + File.separator + dirName;
-            } else {
-                this.carsh_dir = context.getExternalCacheDir().getPath() + File.separator + dirName;
-            }
-        }
-        public static CrashBuilder build(Context context, String dirName) {
-            return new CrashBuilder(context, dirName);
-        }
-
-        @Override
-        public String toString() {
-            return "CarshBuilder [dir path: " + getCrash_dir() + "-- log path:" + getCarsh_log() + "-- tag path:" +
-                    getCrash_tag() + "]";
-        }
-    }
-
     /**
-     * 获取log 日志路径
+     * 获取崩溃头
+     *
+     * @return 崩溃头
      */
-    public static String getLogFilePath() {
-        if (sBuilder == null)
-            return "Unknown";
-        else
-            return sBuilder.getCarsh_log();
-    }
-
-    /**
-     * 获取 LOG 记录的内容
-     */
-    public static String getLogContent() {
-        return FileUtils.readFile2String(getLogFilePath(), "UTF-8");
+    public StringBuilder getCrashHead() {
+        StringBuilder sb = new StringBuilder();
+        try {
+            PackageInfo pi = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
+            sb.append("\n************* Crash Log Head ****************");
+            sb.append("\nDevice Manufacturer: ").append(Build.MANUFACTURER);// 设备厂商
+            sb.append("\nDevice Model       : ").append(Build.MODEL);// 设备型号
+            sb.append("\nAndroid Version    : ").append(Build.VERSION.RELEASE);// 系统版本
+            sb.append("\nAndroid SDK        : ").append(Build.VERSION.SDK_INT);// SDK版本
+            sb.append("\nApp VersionName    : ").append(pi.versionName);
+            sb.append("\nApp VersionCode    : ").append(pi.versionCode);
+            sb.append("\n************* Crash Log Head ****************\n\n");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return sb;
     }
 }
