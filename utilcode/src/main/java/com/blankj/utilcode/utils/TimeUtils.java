98c5d4218a548b2111283472654711b047c5fd42
Blankj <625783482@qq.com>
2016-12-06 23:23:27 +0800

    see 12/06 log

--- utilcode/src/main/java/com/blankj/utilcode/utils/TimeUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/utils/TimeUtils.java
@@ -189,7 +189,7 @@ public class TimeUtils {
      * @return 时间字符串
      */
     public static String millis2String(long millis) {
-        return millis2String(millis, DEFAULT_PATTERN);
+        return new SimpleDateFormat(DEFAULT_PATTERN, Locale.getDefault()).format(new Date(millis));
     }
 
     /**
@@ -1082,8 +1082,8 @@ public class TimeUtils {
         return CHINESE_ZODIAC[year % 12];
     }
 
-    private static final String[] ZODIAC       = {"水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "魔羯座"};
-    private static final int[]    ZODIAC_FLAGS = {20, 19, 21, 21, 21, 22, 23, 23, 23, 24, 23, 22};
+    private static final String[] ZODIAC = {"水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "魔羯座"};
+    private static final int[] ZODIAC_FLAGS = {20, 19, 21, 21, 21, 22, 23, 23, 23, 24, 23, 22};
 
     /**
      * 获取星座
