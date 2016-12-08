31c2b4c181a232c7bc06e7a802bc72bde96a4034
Blankj <625783482@qq.com>
2016-12-08 23:57:12 +0800

    see 12/08 log

---
+++ utilcode/src/main/java/com/blankj/utilcode/utils/Utils.java
@@ -0,0 +1,29 @@
+package com.blankj.utilcode.utils;
+
+import android.content.Context;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 16/12/08
+ *     desc  :
+ * </pre>
+ */
+public class Utils {
+
+    public static Context context;
+
+    private Utils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * 初始化工具类
+     *
+     * @param context 上下文
+     */
+    public static void init(Context context) {
+        Utils.context = context.getApplicationContext();
+    }
+}
