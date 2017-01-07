2d52faf25710661d0ffd8602f562358509f120bf
cmj <625783482@qq.com>
2017-01-07 23:55:10 +0800

    see 01/07 log

--- app/src/main/java/com/blankj/androidutilcode/activities/SnackbarActivity.java
+++ app/src/main/java/com/blankj/androidutilcode/activities/SnackbarActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.blankj.androidutilcode.R;
 import com.blankj.utilcode.utils.SnackbarUtils;
