69b11e123e6f9ba4653f561191ef6ecb5b1d785a
Blankj <625783482@qq.com>
2017-01-18 23:40:20 +0800

    see 01/18 log

---
+++ utilcode/src/main/java/com/blankj/utilcode/utils/FragmentUtils.java
@@ -0,0 +1,14 @@
+package com.blankj.utilcode.utils;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2017/1/17
+ *     desc  :
+ * </pre>
+ */
+public class FragmentUtils {
+
+    
+}
