dde7b210ab2b28fb72e6276370bf33b876b00005
Blankj <625783482@qq.com>
2017-02-18 12:12:54 +0800

    see 02/18 log

--- utilcode/src/main/java/com/blankj/utilcode/utils/KeyboardUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/utils/KeyboardUtils.java
@@ -36,6 +36,19 @@ public class KeyboardUtils {
         View view = activity.getCurrentFocus();
         if (view == null) view = new View(activity);
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * 动态隐藏软键盘
+     *
+     * @param context 上下文
+     * @param view    视图
+     */
+    public static void hideSoftInput(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
