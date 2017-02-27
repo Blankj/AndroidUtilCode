a2e6a935439b456f99a365366f2d168481779794
Blankj <615783482@qq.com>
2017-02-27 19:33:55 +0800

    see 02/27 log

--- utilcode/src/main/java/com/blankj/utilcode/utils/EncryptUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/utils/EncryptUtils.java
@@ -13,9 +13,6 @@ import javax.crypto.Cipher;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import static com.blankj.utilcode.utils.ConvertUtils.bytes2HexString;
-import static com.blankj.utilcode.utils.ConvertUtils.hexString2Bytes;
-
 /**
  * <pre>
  *     author: Blankj
@@ -814,6 +811,68 @@ public class EncryptUtils {
         } catch (Throwable e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    private static final char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    /**
+     * byteArr转hexString
+     * <p>例如：</p>
+     * bytes2HexString(new byte[] { 0, (byte) 0xa8 }) returns 00A8
+     *
+     * @param bytes 字节数组
+     * @return 16进制大写字符串
+     */
+    private static String bytes2HexString(byte[] bytes) {
+        if (bytes == null) return null;
+        int len = bytes.length;
+        if (len <= 0) return null;
+        char[] ret = new char[len << 1];
+        for (int i = 0, j = 0; i < len; i++) {
+            ret[j++] = hexDigits[bytes[i] >>> 4 & 0x0f];
+            ret[j++] = hexDigits[bytes[i] & 0x0f];
+        }
+        return new String(ret);
+    }
+
+
+    /**
+     * hexString转byteArr
+     * <p>例如：</p>
+     * hexString2Bytes("00A8") returns { 0, (byte) 0xA8 }
+     *
+     * @param hexString 十六进制字符串
+     * @return 字节数组
+     */
+    private static byte[] hexString2Bytes(String hexString) {
+        if (StringUtils.isSpace(hexString)) return null;
+        int len = hexString.length();
+        if (len % 2 != 0) {
+            hexString = "0" + hexString;
+            len = len + 1;
+        }
+        char[] hexBytes = hexString.toUpperCase().toCharArray();
+        byte[] ret = new byte[len >> 1];
+        for (int i = 0; i < len; i += 2) {
+            ret[i >> 1] = (byte) (hex2Dec(hexBytes[i]) << 4 | hex2Dec(hexBytes[i + 1]));
+        }
+        return ret;
+    }
+
+    /**
+     * hexChar转int
+     *
+     * @param hexChar hex单个字节
+     * @return 0..15
+     */
+    private static int hex2Dec(char hexChar) {
+        if (hexChar >= '0' && hexChar <= '9') {
+            return hexChar - '0';
+        } else if (hexChar >= 'A' && hexChar <= 'F') {
+            return hexChar - 'A' + 10;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 }
