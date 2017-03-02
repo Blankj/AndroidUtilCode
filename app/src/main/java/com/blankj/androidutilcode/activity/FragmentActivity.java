1fd0c9afbe890390db81d08df76e737300642434
Blankj <625783482@qq.com>
2017-03-02 23:58:34 +0800

    see 03/02 log

--- app/src/main/java/com/blankj/androidutilcode/activity/FragmentActivity.java
+++ app/src/main/java/com/blankj/androidutilcode/activity/FragmentActivity.java
@@ -30,7 +30,7 @@ public class FragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
         ArrayList<Fragment> fragments = new ArrayList<>();
-        fragments.add(Demo0Fragment.newInstance());
+        fragments.add(Demo0Fragment.newInstance()); 
         rootFragment = FragmentUtils.addFragments(getSupportFragmentManager(), fragments, R.id.fragment_container, 0);
     }
 
