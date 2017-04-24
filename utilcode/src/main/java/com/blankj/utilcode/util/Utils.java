62821a33a20ae298110b47068aa2a8276e00efa2
Frank Kwok <ghlfrankkwok@gmail.com>
2017-04-24 13:09:29 +0800

    Add annotations and fix bug in StringUtils.equalsIgnoreCase

--- utilcode/src/main/java/com/blankj/utilcode/util/Utils.java
+++ utilcode/src/main/java/com/blankj/utilcode/util/Utils.java
@@ -11,7 +11,7 @@ import android.content.Context;
  * </pre>
  */
 public final class Utils {
-
+    @SuppressWarnings("StaticFieldLeak")
     private static Context context;
 
     private Utils() {
