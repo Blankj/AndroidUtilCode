da4ed66b25adff0ce7bbf93a08040708a7ba430b
Frank Kwok <ghlfrankkwok@gmail.com>
2017-04-24 11:47:41 +0800

    Fix some typos

--- utilcode/src/main/java/com/blankj/utilcode/constant/TimeConstants.java
+++ utilcode/src/main/java/com/blankj/utilcode/constant/TimeConstants.java
@@ -16,17 +16,17 @@ import java.lang.annotation.RetentionPolicy;
 public final class TimeConstants {
 
     /**
-     * 秒与毫秒的倍数
+     * 毫秒与毫秒的倍数
      */
     public static final int MSEC = 1;
     /**
      * 秒与毫秒的倍数
      */
-    public static final int SEC  = 1000;
+    public static final int SEC = 1000;
     /**
      * 分与毫秒的倍数
      */
-    public static final int MIN  = 60000;
+    public static final int MIN = 60000;
     /**
      * 时与毫秒的倍数
      */
@@ -34,7 +34,7 @@ public final class TimeConstants {
     /**
      * 天与毫秒的倍数
      */
-    public static final int DAY  = 86400000;
+    public static final int DAY = 86400000;
 
     @IntDef({MSEC, SEC, MIN, HOUR, DAY})
     @Retention(RetentionPolicy.SOURCE)
