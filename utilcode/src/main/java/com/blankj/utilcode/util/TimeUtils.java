0cf233732421c4c21c042ae200bc0d7fefdba0ad
Blankj <625783482@qq.com>
2017-05-11 00:26:52 +0800

    see 05/11 log

--- utilcode/src/main/java/com/blankj/utilcode/util/TimeUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/util/TimeUtils.java
@@ -177,7 +177,7 @@ public final class TimeUtils {
      * 注意：SimpleDateFormat不是线程安全的，线程安全需用{@code ThreadLocal<SimpleDateFormat>}
      */
 
-    private static final DateFormat DEFAULT_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+    private static final DateFormat DEFAULT_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
 
     private TimeUtils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
