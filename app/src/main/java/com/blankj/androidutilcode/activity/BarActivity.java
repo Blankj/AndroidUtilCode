ce9deb050c154ae66d8a8a2430f7b15358e93650
Blankj <625783482@qq.com>
2017-05-28 16:38:28 +0800

    see 05/28 log

---
+++ app/src/main/java/com/blankj/androidutilcode/activity/BarActivity.java
@@ -0,0 +1,14 @@
+package com.blankj.androidutilcode.activity;
+
+import com.blankj.androidutilcode.base.BaseActivity;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2017/05/27
+ *     desc  :
+ * </pre>
+ */
+public class BarActivity extends BaseActivity {
+}
