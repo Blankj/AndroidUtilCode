eb1b5ba5c6400eeaeae062beb8152d8aca1838b9
bulasuo <bulasuo@foxmail.com>
2017-09-06 19:39:39 +0800

    bug fig   其实这里的isSpace方法 应该改成 hasSpace

--- utilcode/src/main/java/com/blankj/utilcode/util/AppUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/util/AppUtils.java
@@ -692,10 +692,10 @@ public final class AppUtils {
     private static boolean isSpace(final String s) {
         if (s == null) return true;
         for (int i = 0, len = s.length(); i < len; ++i) {
-            if (!Character.isWhitespace(s.charAt(i))) {
-                return false;
+            if (Character.isWhitespace(s.charAt(i))) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
