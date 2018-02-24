f33518e4a9b4c04bbf5216651a1ad66eb78beb3b
Blankj <625783482@qq.com>
2018-02-24 11:16:07 +0800

    see 02/24 log

--- utilcode/src/main/java/com/blankj/utilcode/util/ConvertUtils.java
+++ utilcode/src/main/java/com/blankj/utilcode/util/ConvertUtils.java
@@ -38,6 +38,81 @@ public final class ConvertUtils {
             {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     /**
+     * Bytes to bits.
+     *
+     * @param bytes The bytes.
+     * @return bits
+     */
+    public static String bytes2Bits(final byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte aByte : bytes) {
+            for (int j = 7; j >= 0; --j) {
+                sb.append(((aByte >> j) & 0x01) == 0 ? '0' : '1');
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Bits to bytes.
+     *
+     * @param bits The bits.
+     * @return bytes
+     */
+    public static byte[] bits2Bytes(String bits) {
+        int lenMod = bits.length() % 8;
+        int byteLen = bits.length() / 8;
+        // add "0" until length to 8 times
+        if (lenMod != 0) {
+            for (int i = lenMod; i < 8; i++) {
+                bits = "0" + bits;
+            }
+            byteLen++;
+        }
+        byte[] bytes = new byte[byteLen];
+        for (int i = 0; i < byteLen; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                bytes[i] <<= 1;
+                bytes[i] |= bits.charAt(i * 8 + j) - '0';
+            }
+        }
+        return bytes;
+    }
+
+    /**
+     * Bytes to chars.
+     *
+     * @param bytes The bytes.
+     * @return chars
+     */
+    public static char[] bytes2Chars(final byte[] bytes) {
+        if (bytes == null) return null;
+        int len = bytes.length;
+        if (len <= 0) return null;
+        char[] chars = new char[len];
+        for (int i = 0; i < len; i++) {
+            chars[i] = (char) (bytes[i] & 0xff);
+        }
+        return chars;
+    }
+
+    /**
+     * Chars to bytes.
+     *
+     * @param chars The chars.
+     * @return bytes
+     */
+    public static byte[] chars2Bytes(final char[] chars) {
+        if (chars == null || chars.length <= 0) return null;
+        int len = chars.length;
+        byte[] bytes = new byte[len];
+        for (int i = 0; i < len; i++) {
+            bytes[i] = (byte) (chars[i]);
+        }
+        return bytes;
+    }
+
+    /**
      * Bytes to hex string.
      * <p>e.g. bytes2HexString(new byte[] { 0, (byte) 0xa8 }) returns "00A8"</p>
      *
@@ -86,39 +161,6 @@ public final class ConvertUtils {
         } else {
             throw new IllegalArgumentException();
         }
-    }
-
-    /**
-     * Chars to bytes.
-     *
-     * @param chars The chars.
-     * @return bytes
-     */
-    public static byte[] chars2Bytes(final char[] chars) {
-        if (chars == null || chars.length <= 0) return null;
-        int len = chars.length;
-        byte[] bytes = new byte[len];
-        for (int i = 0; i < len; i++) {
-            bytes[i] = (byte) (chars[i]);
-        }
-        return bytes;
-    }
-
-    /**
-     * Bytes to chars.
-     *
-     * @param bytes The bytes.
-     * @return chars
-     */
-    public static char[] bytes2Chars(final byte[] bytes) {
-        if (bytes == null) return null;
-        int len = bytes.length;
-        if (len <= 0) return null;
-        char[] chars = new char[len];
-        for (int i = 0; i < len; i++) {
-            chars[i] = (char) (bytes[i] & 0xff);
-        }
-        return chars;
     }
 
     /**
@@ -182,10 +224,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * 以 unit 为单位的时间长度转毫秒时间戳
+     * Time span in unit to milliseconds.
      *
-     * @param timeSpan 毫秒时间戳
-     * @param unit     单位类型
+     * @param timeSpan The time span.
+     * @param unit     The unit of time span.
      *                 <ul>
      *                 <li>{@link TimeConstants#MSEC}</li>
      *                 <li>{@link TimeConstants#SEC }</li>
@@ -193,17 +235,17 @@ public final class ConvertUtils {
      *                 <li>{@link TimeConstants#HOUR}</li>
      *                 <li>{@link TimeConstants#DAY }</li>
      *                 </ul>
-     * @return 毫秒时间戳
+     * @return milliseconds
      */
     public static long timeSpan2Millis(final long timeSpan, @TimeConstants.Unit final int unit) {
         return timeSpan * unit;
     }
 
     /**
-     * 毫秒时间戳转以 unit 为单位的时间长度
+     * Milliseconds to time span in unit.
      *
-     * @param millis 毫秒时间戳
-     * @param unit   单位类型
+     * @param millis The milliseconds.
+     * @param unit   The unit of time span.
      *               <ul>
      *               <li>{@link TimeConstants#MSEC}</li>
      *               <li>{@link TimeConstants#SEC }</li>
@@ -211,27 +253,27 @@ public final class ConvertUtils {
      *               <li>{@link TimeConstants#HOUR}</li>
      *               <li>{@link TimeConstants#DAY }</li>
      *               </ul>
-     * @return 以 unit 为单位的时间长度
+     * @return time span in unit
      */
     public static long millis2TimeSpan(final long millis, @TimeConstants.Unit final int unit) {
         return millis / unit;
     }
 
     /**
-     * 毫秒时间戳转合适时间长度
+     * Milliseconds to fit time span.
      *
-     * @param millis    毫秒时间戳
-     *                  <p>小于等于 0，返回 null</p>
-     * @param precision 精度
+     * @param millis    The milliseconds.
+     *                  <p>millis &lt;= 0, return null</p>
+     * @param precision The precision of time span.
      *                  <ul>
-     *                  <li>precision = 0，返回 null</li>
-     *                  <li>precision = 1，返回天</li>
-     *                  <li>precision = 2，返回天和小时</li>
-     *                  <li>precision = 3，返回天、小时和分钟</li>
-     *                  <li>precision = 4，返回天、小时、分钟和秒</li>
-     *                  <li>precision &gt;= 5，返回天、小时、分钟、秒和毫秒</li>
+     *                  <li>precision = 0, return null</li>
+     *                  <li>precision = 1, return 天</li>
+     *                  <li>precision = 2, return 天, 小时</li>
+     *                  <li>precision = 3, return 天, 小时, 分钟</li>
+     *                  <li>precision = 4, return 天, 小时, 分钟, 秒</li>
+     *                  <li>precision &gt;= 5，return 天, 小时, 分钟, 秒, 毫秒</li>
      *                  </ul>
-     * @return 合适时间长度
+     * @return fit time span
      */
     @SuppressLint("DefaultLocale")
     public static String millis2FitTimeSpan(long millis, int precision) {
@@ -251,52 +293,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * bytes 转 bits
+     * Input stream to output stream.
      *
-     * @param bytes 字节数组
-     * @return bits
-     */
-    public static String bytes2Bits(final byte[] bytes) {
-        StringBuilder sb = new StringBuilder();
-        for (byte aByte : bytes) {
-            for (int j = 7; j >= 0; --j) {
-                sb.append(((aByte >> j) & 0x01) == 0 ? '0' : '1');
-            }
-        }
-        return sb.toString();
-    }
-
-    /**
-     * bits 转 bytes
-     *
-     * @param bits 二进制
-     * @return bytes
-     */
-    public static byte[] bits2Bytes(String bits) {
-        int lenMod = bits.length() % 8;
-        int byteLen = bits.length() / 8;
-        // 不是 8 的倍数前面补 0
-        if (lenMod != 0) {
-            for (int i = lenMod; i < 8; i++) {
-                bits = "0" + bits;
-            }
-            byteLen++;
-        }
-        byte[] bytes = new byte[byteLen];
-        for (int i = 0; i < byteLen; ++i) {
-            for (int j = 0; j < 8; ++j) {
-                bytes[i] <<= 1;
-                bytes[i] |= bits.charAt(i * 8 + j) - '0';
-            }
-        }
-        return bytes;
-    }
-
-    /**
-     * inputStream 转 outputStream
-     *
-     * @param is 输入流
-     * @return outputStream 子类
+     * @param is The input stream.
+     * @return output stream
      */
     public static ByteArrayOutputStream input2OutputStream(final InputStream is) {
         if (is == null) return null;
@@ -317,10 +317,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * outputStream 转 inputStream
+     * Output stream to input stream.
      *
-     * @param out 输出流
-     * @return inputStream 子类
+     * @param out The output stream.
+     * @return input stream
      */
     public ByteArrayInputStream output2InputStream(final OutputStream out) {
         if (out == null) return null;
@@ -328,10 +328,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * inputStream 转 byteArr
+     * Input stream to bytes.
      *
-     * @param is 输入流
-     * @return 字节数组
+     * @param is The input stream.
+     * @return bytes
      */
     public static byte[] inputStream2Bytes(final InputStream is) {
         if (is == null) return null;
@@ -339,10 +339,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * byteArr 转 inputStream
+     * Bytes to input stream.
      *
-     * @param bytes 字节数组
-     * @return 输入流
+     * @param bytes The bytes.
+     * @return input stream
      */
     public static InputStream bytes2InputStream(final byte[] bytes) {
         if (bytes == null || bytes.length <= 0) return null;
@@ -350,10 +350,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * outputStream 转 byteArr
+     * Output stream to bytes.
      *
-     * @param out 输出流
-     * @return 字节数组
+     * @param out The output stream.
+     * @return bytes
      */
     public static byte[] outputStream2Bytes(final OutputStream out) {
         if (out == null) return null;
@@ -361,10 +361,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * outputStream 转 byteArr
+     * Bytes to output stream.
      *
-     * @param bytes 字节数组
-     * @return 字节数组
+     * @param bytes The bytes.
+     * @return output stream
      */
     public static OutputStream bytes2OutputStream(final byte[] bytes) {
         if (bytes == null || bytes.length <= 0) return null;
@@ -382,11 +382,11 @@ public final class ConvertUtils {
     }
 
     /**
-     * inputStream 转 string 按编码
+     * Input stream to string.
      *
-     * @param is          输入流
-     * @param charsetName 编码格式
-     * @return 字符串
+     * @param is          The input stream.
+     * @param charsetName The name of charset.
+     * @return string
      */
     public static String inputStream2String(final InputStream is, final String charsetName) {
         if (is == null || isSpace(charsetName)) return null;
@@ -399,11 +399,11 @@ public final class ConvertUtils {
     }
 
     /**
-     * string 转 inputStream 按编码
+     * String to input stream.
      *
-     * @param string      字符串
-     * @param charsetName 编码格式
-     * @return 输入流
+     * @param string      The string.
+     * @param charsetName The name of charset.
+     * @return input stream
      */
     public static InputStream string2InputStream(final String string, final String charsetName) {
         if (string == null || isSpace(charsetName)) return null;
@@ -416,11 +416,11 @@ public final class ConvertUtils {
     }
 
     /**
-     * outputStream 转 string 按编码
+     * Output stream to string.
      *
-     * @param out         输出流
-     * @param charsetName 编码格式
-     * @return 字符串
+     * @param out         The output stream.
+     * @param charsetName The name of charset.
+     * @return string
      */
     public static String outputStream2String(final OutputStream out, final String charsetName) {
         if (out == null || isSpace(charsetName)) return null;
@@ -433,11 +433,11 @@ public final class ConvertUtils {
     }
 
     /**
-     * string 转 outputStream 按编码
+     * String to output stream.
      *
-     * @param string      字符串
-     * @param charsetName 编码格式
-     * @return 输入流
+     * @param string      The string.
+     * @param charsetName The name of charset.
+     * @return output stream
      */
     public static OutputStream string2OutputStream(final String string, final String charsetName) {
         if (string == null || isSpace(charsetName)) return null;
@@ -450,11 +450,11 @@ public final class ConvertUtils {
     }
 
     /**
-     * bitmap 转 byteArr
+     * Bitmap to bytes.
      *
-     * @param bitmap bitmap 对象
-     * @param format 格式
-     * @return 字节数组
+     * @param bitmap The bitmap.
+     * @param format The format of bitmap.
+     * @return bytes
      */
     public static byte[] bitmap2Bytes(final Bitmap bitmap, final Bitmap.CompressFormat format) {
         if (bitmap == null) return null;
@@ -464,9 +464,9 @@ public final class ConvertUtils {
     }
 
     /**
-     * byteArr 转 bitmap
+     * Bytes to bitmap.
      *
-     * @param bytes 字节数组
+     * @param bytes The bytes.
      * @return bitmap
      */
     public static Bitmap bytes2Bitmap(final byte[] bytes) {
@@ -476,9 +476,9 @@ public final class ConvertUtils {
     }
 
     /**
-     * drawable 转 bitmap
+     * Drawable to bitmap.
      *
-     * @param drawable drawable 对象
+     * @param drawable The drawable.
      * @return bitmap
      */
     public static Bitmap drawable2Bitmap(final Drawable drawable) {
@@ -508,9 +508,9 @@ public final class ConvertUtils {
     }
 
     /**
-     * bitmap 转 drawable
+     * Bitmap to drawable.
      *
-     * @param bitmap bitmap 对象
+     * @param bitmap The bitmap.
      * @return drawable
      */
     public static Drawable bitmap2Drawable(final Bitmap bitmap) {
@@ -518,11 +518,11 @@ public final class ConvertUtils {
     }
 
     /**
-     * drawable 转 byteArr
+     * Drawable to bytes.
      *
-     * @param drawable drawable 对象
-     * @param format   格式
-     * @return 字节数组
+     * @param drawable The drawable.
+     * @param format   The format of bitmap.
+     * @return bytes
      */
     public static byte[] drawable2Bytes(final Drawable drawable,
                                         final Bitmap.CompressFormat format) {
@@ -530,9 +530,9 @@ public final class ConvertUtils {
     }
 
     /**
-     * byteArr 转 drawable
+     * Bytes to drawable.
      *
-     * @param bytes 字节数组
+     * @param bytes The bytes.
      * @return drawable
      */
     public static Drawable bytes2Drawable(final byte[] bytes) {
@@ -540,9 +540,9 @@ public final class ConvertUtils {
     }
 
     /**
-     * view 转 Bitmap
+     * View to bitmap.
      *
-     * @param view 视图
+     * @param view The view.
      * @return bitmap
      */
     public static Bitmap view2Bitmap(final View view) {
@@ -561,10 +561,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * dp 转 px
+     * Value of dp to value of px.
      *
-     * @param dpValue dp 值
-     * @return px 值
+     * @param dpValue The value of dp.
+     * @return value of px
      */
     public static int dp2px(final float dpValue) {
         final float scale = Utils.getApp().getResources().getDisplayMetrics().density;
@@ -572,10 +572,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * px 转 dp
+     * Value of px to value of dp.
      *
-     * @param pxValue px 值
-     * @return dp 值
+     * @param pxValue The value of px.
+     * @return value of dp
      */
     public static int px2dp(final float pxValue) {
         final float scale = Utils.getApp().getResources().getDisplayMetrics().density;
@@ -583,10 +583,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * sp 转 px
+     * Value of sp to value of px.
      *
-     * @param spValue sp 值
-     * @return px 值
+     * @param spValue The value of sp.
+     * @return value of px
      */
     public static int sp2px(final float spValue) {
         final float fontScale = Utils.getApp().getResources().getDisplayMetrics().scaledDensity;
@@ -594,10 +594,10 @@ public final class ConvertUtils {
     }
 
     /**
-     * px 转 sp
+     * Value of px to value of sp.
      *
-     * @param pxValue px 值
-     * @return sp 值
+     * @param pxValue The value of px.
+     * @return value of sp
      */
     public static int px2sp(final float pxValue) {
         final float fontScale = Utils.getApp().getResources().getDisplayMetrics().scaledDensity;
