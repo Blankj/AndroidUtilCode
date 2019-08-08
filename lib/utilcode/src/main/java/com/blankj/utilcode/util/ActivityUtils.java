6e27c6ec4c11e5600f3b40f22b536f96a7483f72
Blankj <625783482@qq.com>
2019-08-09 01:15:51 +0800

    see 08/09 log

--- lib/utilcode/src/main/java/com/blankj/utilcode/util/ActivityUtils.java
+++ lib/utilcode/src/main/java/com/blankj/utilcode/util/ActivityUtils.java
@@ -15,9 +15,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -1054,7 +1056,9 @@ public final class ActivityUtils {
      * @param pkg The name of the package.
      */
     public static void startLauncherActivity(@NonNull final String pkg) {
-        startActivity(pkg, getLauncherActivity(pkg));
+        String launcherActivity = getLauncherActivity(pkg);
+        if (TextUtils.isEmpty(launcherActivity)) return;
+        startActivity(pkg, launcherActivity);
     }
 
     /**
@@ -1087,11 +1091,47 @@ public final class ActivityUtils {
         intent.setPackage(pkg);
         PackageManager pm = Utils.getApp().getPackageManager();
         List<ResolveInfo> info = pm.queryIntentActivities(intent, 0);
-        ResolveInfo next = info.iterator().next();
-        if (next != null) {
-            return next.activityInfo.name;
+        int size = info.size();
+        if (size == 0) return "";
+        for (int i = 0; i < size; i++) {
+            ResolveInfo ri = info.get(i);
+            if (ri.activityInfo.processName.equals(pkg)) {
+                return ri.activityInfo.name;
+            }
         }
-        return "no launcher activity of " + pkg;
+        return info.get(0).activityInfo.name;
+    }
+
+    /**
+     * Return the list of main activities.
+     *
+     * @return the list of main activities
+     */
+    public static List<String> getMainActivities() {
+        return getMainActivities(Utils.getApp().getPackageName());
+    }
+
+    /**
+     * Return the list of main activities.
+     *
+     * @param pkg The name of the package.
+     * @return the list of main activities
+     */
+    public static List<String> getMainActivities(@NonNull final String pkg) {
+        List<String> ret = new ArrayList<>();
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.setPackage(pkg);
+        PackageManager pm = Utils.getApp().getPackageManager();
+        List<ResolveInfo> info = pm.queryIntentActivities(intent, 0);
+        int size = info.size();
+        if (size == 0) return ret;
+        for (int i = 0; i < size; i++) {
+            ResolveInfo ri = info.get(i);
+            if (ri.activityInfo.processName.equals(pkg)) {
+                ret.add(ri.activityInfo.name);
+            }
+        }
+        return ret;
     }
 
     /**
--- lib/utilcode/src/main/java/com/blankj/utilcode/util/AppUtils.java
+++ lib/utilcode/src/main/java/com/blankj/utilcode/util/AppUtils.java
@@ -6,6 +6,7 @@ import android.app.ActivityManager;
 import android.app.AppOpsManager;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -293,7 +294,12 @@ public final class AppUtils {
      */
     public static void launchApp(final String packageName) {
         if (isSpace(packageName)) return;
-        Utils.getApp().startActivity(getLaunchAppIntent(packageName, true));
+        Intent launchAppIntent = getLaunchAppIntent(packageName, true);
+        if (launchAppIntent == null) {
+            Log.e("AppUtils", "Didn't exist launcher activity.");
+            return;
+        }
+        Utils.getApp().startActivity(launchAppIntent);
     }
 
     /**
@@ -308,7 +314,12 @@ public final class AppUtils {
                                  final String packageName,
                                  final int requestCode) {
         if (isSpace(packageName)) return;
-        activity.startActivityForResult(getLaunchAppIntent(packageName), requestCode);
+        Intent launchAppIntent = getLaunchAppIntent(packageName);
+        if (launchAppIntent == null) {
+            Log.e("AppUtils", "Didn't exist launcher activity.");
+            return;
+        }
+        activity.startActivityForResult(launchAppIntent, requestCode);
     }
 
     /**
@@ -324,9 +335,11 @@ public final class AppUtils {
      * @param isKillProcess True to kill the process, false otherwise.
      */
     public static void relaunchApp(final boolean isKillProcess) {
-        PackageManager packageManager = Utils.getApp().getPackageManager();
-        Intent intent = packageManager.getLaunchIntentForPackage(Utils.getApp().getPackageName());
-        if (intent == null) return;
+        Intent intent = getLaunchAppIntent(Utils.getApp().getPackageName(), true);
+        if (intent == null) {
+            Log.e("AppUtils", "Didn't exist launcher activity.");
+            return;
+        }
         intent.addFlags(
                 Intent.FLAG_ACTIVITY_NEW_TASK
                         | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK
@@ -914,9 +927,32 @@ public final class AppUtils {
     }
 
     private static Intent getLaunchAppIntent(final String packageName, final boolean isNewTask) {
-        Intent intent = Utils.getApp().getPackageManager().getLaunchIntentForPackage(packageName);
-        if (intent == null) return null;
-        return isNewTask ? intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) : intent;
+        String launcherActivity = getLauncherActivity(packageName);
+        if (!launcherActivity.isEmpty()) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            ComponentName cn = new ComponentName(packageName, launcherActivity);
+            intent.setComponent(cn);
+            return isNewTask ? intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) : intent;
+        }
+        return null;
+    }
+
+    private static String getLauncherActivity(@NonNull final String pkg) {
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setPackage(pkg);
+        PackageManager pm = Utils.getApp().getPackageManager();
+        List<ResolveInfo> info = pm.queryIntentActivities(intent, 0);
+        int size = info.size();
+        if (size == 0) return "";
+        for (int i = 0; i < size; i++) {
+            ResolveInfo ri = info.get(i);
+            if (ri.activityInfo.processName.equals(pkg)) {
+                return ri.activityInfo.name;
+            }
+        }
+        return info.get(0).activityInfo.name;
     }
 
     private static String getForegroundProcessName() {
