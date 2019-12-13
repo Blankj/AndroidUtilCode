dfa36bb8bc1ca7d97bbf2c48b0b50132631ef2f5
liyujiang <1032694760@qq.com>
2019-12-13 15:17:49 +0800

    传入字符串类型的文件路径，判断文件是否存在在Android10.0系统上是存在严重问题的

--- lib/utilcode/src/main/java/com/blankj/utilcode/util/FileUtils.java
+++ lib/utilcode/src/main/java/com/blankj/utilcode/util/FileUtils.java
@@ -61,23 +61,24 @@ public final class FileUtils {
      * @return {@code true}: yes<br>{@code false}: no
      */
     public static boolean isFileExists(final String filePath) {
-        if (Build.VERSION.SDK_INT < 29) {
-            return isFileExists(getFileByPath(filePath));
-        } else {
-            try {
-                Uri uri = Uri.parse(filePath);
-                ContentResolver cr = Utils.getApp().getContentResolver();
-                AssetFileDescriptor afd = cr.openAssetFileDescriptor(uri, "r");
-                if (afd == null) return false;
-                try {
-                    afd.close();
-                } catch (IOException ignore) {
-                }
-            } catch (FileNotFoundException e) {
-                return false;
-            }
+        boolean fileExists = isFileExists(getFileByPath(filePath));
+        if (fileExists){
             return true;
         }
+        // Compatible API 29+ as follow
+        try {
+            Uri uri = Uri.parse(filePath);
+            ContentResolver cr = Utils.getApp().getContentResolver();
+            AssetFileDescriptor afd = cr.openAssetFileDescriptor(uri, "r");
+            if (afd == null) return false;
+            try {
+                afd.close();
+            } catch (IOException ignore) {
+            }
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+        return true;
     }
 
     /**
