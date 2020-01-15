1a3a1f62a7ee51ddf63189b830ded4e8df2ff184
crmtx <r.cen@maimemo.com>
2020-01-15 20:06:33 +0800

    修正BarUtils错误

--- lib/utilcode/src/main/java/com/blankj/utilcode/util/BarUtils.java
+++ lib/utilcode/src/main/java/com/blankj/utilcode/util/BarUtils.java
@@ -651,7 +651,7 @@ public final class BarUtils {
      */
     public static void setNavBarLightMode(@NonNull final Activity activity,
                                           final boolean isLightMode) {
-        setStatusBarLightMode(activity.getWindow(), isLightMode);
+        setNavBarLightMode(activity.getWindow(), isLightMode);
     }
 
     /**
@@ -681,7 +681,7 @@ public final class BarUtils {
      * @return {@code true}: yes<br>{@code false}: no
      */
     public static boolean isNavBarLightMode(@NonNull final Activity activity) {
-        return isStatusBarLightMode(activity.getWindow());
+        return isNavBarLightMode(activity.getWindow());
     }
 
     /**
