74155c92ca81242a6ec80bf53a162fa7997e2023
Blankj <blankj@qq.com>
2020-10-29 20:03:20 +0800

    see 10/29 log

--- buildSrc/src/main/groovy/Config.groovy
+++ buildSrc/src/main/groovy/Config.groovy
@@ -14,8 +14,8 @@ class Config {
     static compileSdkVersion = 29
     static minSdkVersion = 14
     static targetSdkVersion = 29
-    static versionCode = 1_030_003
-    static versionName = '1.30.3'// E.g. 1.9.72 => 1,009,072
+    static versionCode = 1_030_004
+    static versionName = '1.30.4'// E.g. 1.9.72 => 1,009,072
 
     // lib version
     static gradlePluginVersion = '3.5.0'
